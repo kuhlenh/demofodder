8813873d136afb349220c2ba75b39f0edfdf3a9b
kuhlenh <kaseyu@microsoft.com>
2016-08-12 10:06:07 -0700

    added start of analyzer demo

---
+++ IOperationZeroSizeArrayAnalyzer_Start/ZeroSizeArrayAnalyzer/ZeroSizeArrayAnalyzer.Test/UnitTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestHelper;
+using ZeroSizeArrayAnalyzer;
+using Xunit;
+
+namespace ZeroSizeArrayAnalyzer.Test
+{
+
+    public class UnitTest : CodeFixVerifier
+    {
+
+        //No diagnostics expected to show up
+        [Fact]
+        public void TestMethod1()
+        {
+            var test = @"";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [Fact]
+        public void TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {   
+            string[] a = new string[0];
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "ZeroSizeArrayAnalyzer",
+                Message = "Don't create a zero-length array.",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 13, 26)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+
+            var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new ZeroSizeArrayAnalyzerCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new ZeroSizeArrayAnalyzerAnalyzer();
+        }
+    }
+}
---
+++ IOperationZeroSizeArrayAnalyzer_Start/ZeroSizeArrayAnalyzer/ZeroSizeArrayAnalyzer/CodeFixProvider.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Semantics;
+
+namespace ZeroSizeArrayAnalyzer
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, LanguageNames.VisualBasic, Name = nameof(ZeroSizeArrayAnalyzerCodeFixProvider)), Shared]
+    public class ZeroSizeArrayAnalyzerCodeFixProvider : CodeFixProvider
+    {
+        private const string title = "Use Array.Empty<T>";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(ZeroSizeArrayAnalyzerAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var diagnostic = context.Diagnostics[0];
+            var span = diagnostic.Location.SourceSpan;
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: title,
+                    createChangedDocument: c => UseArrayEmptyAsync(c, context.Document, span),
+                    equivalenceKey: title),
+                diagnostic);
+
+            return Task.FromResult(false);
+        }
+
+        private async Task<Document> UseArrayEmptyAsync(CancellationToken cancellationToken, Document document, TextSpan diagnosticSpan)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            
+            var arrayCreation = root.FindNode(diagnosticSpan);
+
+            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+            var arrayOperation = (IArrayCreationExpression)semanticModel.GetOperation(arrayCreation);
+
+            // Construct syntax for code fix
+            var generator = SyntaxGenerator.GetGenerator(document);
+            var arrayTypeExpression = generator.TypeExpression(semanticModel.Compilation.GetTypeByMetadataName("System.Array"));
+            var memberName = generator.GenericName("Empty", arrayOperation.ElementType);
+            var memberAccess = generator.MemberAccessExpression(arrayTypeExpression, memberName);
+            var invocationExpression = generator.InvocationExpression(memberAccess);
+            
+            var newRoot = root.ReplaceNode(arrayCreation, invocationExpression);
+            var newDocument = document.WithSyntaxRoot(newRoot);
+
+            return newDocument;
+        }
+    }
+}
